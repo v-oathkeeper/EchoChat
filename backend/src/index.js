950de65643a1ec0a2fefb0cd4831a4462f8a03c0
v-oathkeeper <vishnuvichu8150@gmail.com>
2025-04-01 15:13:25 +0530

    Added message routes and controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from './lib/db.js';
 
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on port: " + PORT);
