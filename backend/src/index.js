ad9fc9238722c54f065a7d292dfc18d11733e0ea
v-oathkeeper <vishnuvichu8150@gmail.com>
2025-04-04 21:45:32 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,14 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from './lib/db.js';
 import cors from 'cors';
 import { app, server } from './lib/socket.js';
+import path from 'path';
 
 dotenv.config();
 
 
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,6 +26,15 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 
 server.listen(PORT, () => {
     console.log("server is running on port: " + PORT);
